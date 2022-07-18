4075274314c905c9c7c8ac960da9cd722193f671
artShek123 <arturshekikyan@gmail.com>
2022-07-18 11:18:41 +0400

    lesson 14 home

---
+++ my_app/src/components/Confirm.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import PropTypes from 'prop-types'
+
+function Confirm(props) {
+  return (
+    <Modal
+      {...props}
+      confirmdeleting={props.confirmdeleting}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.confirmtext}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Footer>
+        <Button 
+            onClick={() => {
+                    props.confirmtext === 'Are you sure to remove the task ?'
+                    ?
+                        props.confirmdeleting(props.taskid)
+                    :
+                        props.confirmdeleting()
+                }
+            }
+            variant='danger'
+        >
+            Delete
+        </Button>
+        <Button onClick={props.onHide}>Cancel</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+Confirm.propTypes = {
+    show: PropTypes.bool.isRequired,
+    onHide: PropTypes.func.isRequired,
+    confirmdeleting: PropTypes.func.isRequired,
+    taskid: PropTypes.number.isRequired,
+    confirmtext: PropTypes.string.isRequired
+};
+
+export default Confirm
